726c66ae3a5b9f2fe15a7ed8b51aacf78a21182a
Aktiv-Princi-Shah <princi.shah@aktivsoftware.com>
2025-07-31 16:22:35 +0530

    file submission

---
+++ ak_library_management/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'ak_library_management',
+    'version': '18.0.1.0.0',
+    'author': 'Princi',
+    'website': 'https://www.prihhhh.com',
+    'category': 'Library',
+    'depends': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/library_book_views.xml',
+        'views/library_member_views.xml',
+        'views/library_menu_action_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
